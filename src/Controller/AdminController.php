35dd7a69d55a6cac03106c590b25e2d4b96b9adf
FrozenCell239 <val8ioks@gmail.com>
2024-05-02 15:33:15 +0200

    Routes - Added admin index page and AdminController.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin_index')]
+    public function index(): Response
+    {
+        # Redirect non logged-in users
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_index');
+        };
+
+        return $this->render('admin/index.html.twig', [
+            // Datas here
+        ]);
+    }
+}
