5c0106da4379dd4c7b6374f1e626730caacb6c19
FrozenCell239 <val8ioks@gmail.com>
2024-05-02 21:25:27 +0200

    Routes - Added posts CRUD. /!\ Image upload feature is still not working yet.

---
+++ src/Controller/PostController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Image;
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use App\Service\PictureService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/post')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'app_post_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        PictureService $pictureService
+    ): Response
+    {
+        # Redirect non logged-in users
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_index');
+        };
+
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            # Getting images
+            $image = $form->get('attached_picture')->getData();
+            dd($image);
+            if($image){
+                $folder = 'products'; //Destination folder.
+                $file = $pictureService->addPicture($image, $folder, 300, 300); //Calling images adding service.
+                $new_image = new Image();
+                $new_image->setFilename($file);
+                $post->setAttachedPicture($new_image);
+            };
+
+            # Putting the new post into database
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('post/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        # Redirect non logged-in users
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_index');
+        };
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        # Redirect non logged-in users
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_index');
+        };
+
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use App\Entity\Post;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Image as ConstraintsImage;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('place')
+            ->add('date_time', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('content')
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+                'attr' => ['hidden' => 'true']
+            ])
+            ->add('attached_picture', FileType::class, [
+                'multiple' => true,
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new All(
+                        new ConstraintsImage([
+                            'maxWidth' => 1920,
+                            'maxWidthMessage' => 'Le format de l\'image ne doit pas dépasser {{ max_width }} pixels de large.'
+                        ])
+                    )
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
